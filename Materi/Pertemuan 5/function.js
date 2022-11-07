4bf710a0b518eea077dd628e6a4c9f3a61329e6d
AfnanSiregar <francafnan@gmail.com>
2022-11-08 01:39:47 +0700

    push

---
+++ Materi/Pertemuan 5/function.js
@@ -0,0 +1,13 @@
+var firstValue = 0;
+let secondValue = 0;
+const thirdValue = 0;
+
+if (true) {
+    var firstValue = 1;
+    let secondValue = 1;
+    const thirdValue = 1;
+}
+
+console.log(firstValue);
+console.log(secondValue);
+console.log(thirdValue);
